946e64c55535c8236f0b929416795ed2b2e8c599
kennlw <109991402+kennlw@users.noreply.github.com>
2022-08-03 09:04:15 -0400

    Update ChineseTikTokCrawler.py

--- ChineseTikTokCrawler.py
+++ ChineseTikTokCrawler.py
@@ -35,68 +35,85 @@ imVidKeys = [
   "forward_count",
   "whatsapp_share_count"
 ]
+
+keywords = [
+  "早上好", 
+  "好看", 
+  "酷", 
+  "大家", 
+  "特别", 
+  "已经", 
+  "虽然", 
+  "而且", 
+  "不但", 
+  "无法", 
+  "不定", 
+  "这么", 
+  "确定", 
+  "人家", 
+  "感觉",
+  "他们",
+  "我们",
+  "谁想", 
+  "来了", 
+  "现在",
+  "这是",
+  "这里",
+  "每个",
+  "最后",
+  "我被"
+]
 url = "https://tiktok-private1.p.rapidapi.com/search/video"
-
-
-# Insert key here
 headers = {
 "X-RapidAPI-Key": "451607b1e0msh25b2412730c45c8p11508djsn9676e0259193",
 "X-RapidAPI-Host": "tiktok-private1.p.rapidapi.com"
 }
 
 offset = 0
-count = 1
+count = 20
 json_count = 0
 user_ls = []
 vid_ls = []
 json_dict = {}
 
-for i in range(101):
-  kw_ls = ["早上好，好看，酷，大家，特别，已经，虽然，而且，不但，无法"]
-  def keyword():
-    if 0 <= count <= 100:
-      return(kw_ls[0])
-    if 101 <= count <= 200:
-      return(kw_ls[1])
-    if 201 <= count <= 300:
-      return(kw_ls[2])
-    if 301 <= count <= 400:
-      return(kw_ls[3])
-    if 401 <= count <= 500:
-      return(kw_ls[4])
-    if 501 <= count <= 600:
-      return(kw_ls[5])
-    if 601 <= count <= 700:
-      return(kw_ls[6]) 
+for keyword in keywords:
 
-  try:
+    offset = 0
+    for i in range(10):
+
+
+        try:
     
-    querystring = {"keyword": str(keyword()), "offset": str(offset), "count":str(count)}
-    json_response = requests.request("GET", url, headers=headers, params=querystring).json()
-    response = json_response["aweme_list"]
+            querystring = {"keyword": keyword, "offset": str(offset), "count":str(count)}
+            json_response = requests.request("GET", url, headers=headers, params=querystring).json()
+            response = json_response["aweme_list"]
     
-    for x in response:
-      user_ls.append({k:x["author"][k] for k in imAuthKeys})
-      vid_ls.append({y:x["statistics"][y] for y in imVidKeys})
-    
-    vid_dict = vid_ls[i]
-    vid_dict["desc"] = x["desc"]
+            for x in response:
+                user_ls.append({k:x["author"][k] for k in imAuthKeys})
 
-    json_dict[str(json_count)] = json_response
-    json_count+=1
+                vid_dict = {y:x["statistics"][y] for y in imVidKeys}
+                vid_dict["desc"] = x["desc"]
+                vid_ls.append(vid_dict)
 
-  except IndexError: print("Index Error")
-  except KeyError: print("Key Error")
+        except IndexError: print("Index Error")
+        except KeyError: print("Key Error")
+        except requests.exceptions.RequestException as e: print("Request Error")
+        except Exception as e: print("Catch-all error", e)
 
-  offset+=count
-  time.sleep(2)
+        offset+=count
+        time.sleep(70)
 
-  
+
 df1 = pd.DataFrame(user_ls)
 df2 = pd.DataFrame(vid_ls)
 
 userData = df1.to_csv('ChineseTikTokUserData.csv', index = True)
 vidData = df2.to_csv('ChineseTikTokCrawlerVidData.csv', index = True)
 
-with open("rawJson.json", 'w') as f:
-    json.dump(jsonDict, f)
+jsonUserDict = userData[0]
+jsonVidDict = vidData[0]
+
+with open("rawUserJson.json", 'w') as g:
+    json.dump(jsonUserDict, g)
+with open("rawVidJson.json", 'w') as f:
+    json.dump(jsonVidDict, f)
